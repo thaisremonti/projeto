0ef05829024e3ed2ca88857d60f578074969c025
Thais Remonti <thais.remonti@mv.com.br>
2020-02-11 13:08:17 -0300

    criando rotas

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { LoginModule } from './autenticacao/';
+// import { LoginModule } from './autenticacao/';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { LoginModule } from './autenticacao/';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    LoginModule
+    // LoginModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projeto/src/app/autenticacao/login/components/logar.component.ts
@@ -0,0 +1,2 @@
+export class Logar.Component {
+}
